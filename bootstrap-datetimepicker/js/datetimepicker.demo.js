53fb1aa3ff9cc0cf7f8ab3911d026f5a4ccaed20
Bulain Peng <bulain@gmail.com>
2014-10-08 15:45:50 +0800

    add bootstrap-datetimepicker demo

---
+++ bootstrap-datetimepicker/js/datetimepicker.demo.js
@@ -0,0 +1,36 @@
+$(function() {
+    $('.form_datetime').datetimepicker({
+        language: 'zh-CN',
+        format: 'yyyy-mm-dd hh:ii:ss',
+        weekStart : 1,
+        todayBtn : 1,
+        autoclose : 1,
+        todayHighlight : 1,
+        startView : 2,
+        forceParse : 0,
+        showMeridian : 1
+    });
+    $('.form_date').datetimepicker({
+        language: 'zh-CN',
+        format: 'yyyy-mm-dd',
+        weekStart : 1,
+        todayBtn : 1,
+        autoclose : 1,
+        todayHighlight : 1,
+        startView : 2,
+        minView : 2,
+        forceParse : 0
+    });
+    $('.form_time').datetimepicker({
+        language: 'zh-CN',
+        format: 'hh:ii',
+        weekStart : 1,
+        todayBtn : 1,
+        autoclose : 1,
+        todayHighlight : 1,
+        startView : 1,
+        minView : 0,
+        maxView : 1,
+        forceParse : 0
+    });
+});
