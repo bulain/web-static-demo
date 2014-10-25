f670a97bf8900d81ccaf96b59b4ba1316aacab73
Bulain Peng <bulain@gmail.com>
2014-10-25 11:35:17 +0800

    add backbone example

---
+++ backbone/js/todos/main.js
@@ -0,0 +1,13 @@
+require.config({
+    paths : {
+        jquery : "/web-static/jquery/js/jquery",
+        underscore : "/web-static/underscore/js/underscore",
+        backbone : "/web-static/backbone/js/backbone",
+        localstorage : "../backbone.localStorage",
+        todo : "todo",
+    }
+}); 
+
+require(['todo'], function(){
+    
+});
