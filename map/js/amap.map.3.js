299d8adf1ab03606bd2d8288661acff4733f02ac
Bulain Peng <bulain@gmail.com>
2015-06-29 17:33:41 +0800

    update amap for track back

--- map/js/amap.map.3.js
+++ map/js/amap.map.3.js
@@ -68,11 +68,6 @@ AMap.service('AMap.Driving', function() {
             });
             mapObj.setFitView();
 
-            console.log(lines.length);
-
-            var start = lines[0];
-            var log = [];
-            var cnt = 0;
             var marker = new AMap.Marker({
                 map : mapObj,
                 icon : new AMap.Icon({
@@ -83,6 +78,36 @@ AMap.service('AMap.Driving', function() {
                 position : start,
                 offset : new AMap.Pixel(0, -37)
             });
+            
+            var x = [];
+            lines.forEach(function(d){
+                if(x.length == 0 ){
+                    x.push(d);
+                }else{
+                    if(d.distance(x[x.length-1]) > 50){
+                        x.push(d);
+                    }
+                }
+            });
+            console.log(x);
+            
+            markerMovingControl = new MarkerMovingControl(mapObj, marker, x);
+
+            // console.log(lines.length);
+            //
+            // var start = lines[0];
+            // var log = [];
+            // var cnt = 0;
+            // var marker = new AMap.Marker({
+            // map : mapObj,
+            // icon : new AMap.Icon({
+            // image : "http://webapi.amap.com/images/marker.png",
+            // size : new AMap.Size(58, 40),
+            // imageOffset : new AMap.Pixel(0, 0)
+            // }),
+            // position : start,
+            // offset : new AMap.Pixel(0, -37)
+            // });
             /*AMap.event.addListener(marker, "moveend", function() {
             console.log(log.length);
             });*/
@@ -111,48 +136,127 @@ AMap.service('AMap.Driving', function() {
 
              move();*/
 
-            var bool = true;
-            var all = [];
-            var rest = [];
-            var curr;
-            
-            var points = lines.map(function(d){
-                return [d.lng, d.lat];
-            });
+            /*var bool = true;
+             var all = [];
+             var rest = [];
+             var curr;
 
-            marker.moveAlong(lines, 10000, function(d) {
-                console.log(d);
-                var p = marker.getPosition();
-                all.push(p);
-                if (!bool) {
-                    rest.push(p);
-                }
-            }, false);
+             var points = lines.map(function(d){
+             return [d.lng, d.lat];
+             });
 
-            setTimeout(function() {
-                //marker.stopMove();
-                bool = false;
-                curr = marker.getPosition();
-                marker.hide();
-                
-                var x = new AMap.Marker({
-                    map : mapObj,
-                    icon : new AMap.Icon({
-                        image : "http://webapi.amap.com/images/marker.png",
-                        size : new AMap.Size(58, 40),
-                        imageOffset : new AMap.Pixel(0, 0)
-                    }),
-                    position : curr,
-                    offset : new AMap.Pixel(0, -37)
-                });
-                
-            }, 5000);
+             marker.moveAlong(lines, 10000, function(d) {
+             console.log(d);
+             var p = marker.getPosition();
+             all.push(p);
+             if (!bool) {
+             rest.push(p);
+             }
+             }, false);
 
-            AMap.event.addListener(marker, "movealong", function() {
-                console.log(rest);
-                marker.show();
-                //marker.setPosition(curr);
-            });
+             setTimeout(function() {
+             //marker.stopMove();
+             bool = false;
+             curr = marker.getPosition();
+             marker.hide();
+
+             var x = new AMap.Marker({
+             map : mapObj,
+             icon : new AMap.Icon({
+             image : "http://webapi.amap.com/images/marker.png",
+             size : new AMap.Size(58, 40),
+             imageOffset : new AMap.Pixel(0, 0)
+             }),
+             position : curr,
+             offset : new AMap.Pixel(0, -37)
+             });
+
+             }, 5000);
+
+             AMap.event.addListener(marker, "movealong", function() {
+             console.log(rest);
+             marker.show();
+             //marker.setPosition(curr);
+             });
+             */
         }
     });
 });
+
+/**     第一部分，动画暂停、继续的实现     */
+
+/**
+ * Marker移动控件
+ * @param {Map} map    地图对象
+ * @param {Marker} marker Marker对象
+ * @param {Array} path   移动的路径，以坐标数组表示
+ */
+var MarkerMovingControl = function(map, marker, path) {
+    this._map = map;
+    this._marker = marker;
+    this._path = path;
+    this._currentIndex = 0;
+    marker.setMap(map);
+    marker.setPosition(path[0]);
+}
+/**
+ * 移动marker，会从当前位置开始向前移动
+ */
+MarkerMovingControl.prototype.move = function() {
+    if (!this._listenToStepend) {
+        this._listenToStepend = AMap.event.addListener(this, 'stepend', function() {
+            this.step();
+        }, this);
+    }
+    this.step();
+};
+
+/**
+ * 停止移动marker，由于控件会记录当前位置，所以相当于暂停
+ */
+MarkerMovingControl.prototype.stop = function() {
+    this._marker.stopMove();
+};
+
+/**
+ * 重新开始，会把marker移动到路径的起点然后开始移动
+ */
+MarkerMovingControl.prototype.restart = function() {
+    this.stop();
+    this._marker.setPosition(this._path[0]);
+    this._currentIndex = 0;
+    this.move();
+};
+
+/**
+ * 向前移动一步
+ */
+MarkerMovingControl.prototype.step = function() {
+    var nextIndex = this._currentIndex + 1;
+    if (nextIndex < this._path.length) {
+        if (!this._listenToMoveend) {
+            this._listenToMoveend = AMap.event.addListener(this._marker, 'moveend', function() {
+                this._currentIndex++;
+                AMap.event.trigger(this, 'stepend');
+            }, this);
+        }
+        // console.log(nextIndex);
+        this._marker.moveTo(this._path[nextIndex], 100000);
+    }
+};
+
+// 创建移动控件
+var markerMovingControl;
+
+// 编写事件响应函数
+function startAnimation() {
+    markerMovingControl.restart();
+}
+
+function pauseAnimation() {
+    markerMovingControl.stop();
+}
+
+function continueAnimation() {
+    markerMovingControl.move();
+}
