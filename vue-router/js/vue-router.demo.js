f0b47ae70c98d609cd7caa447795ad8068b38895
Bulain Peng <bulain@gmail.com>
2017-04-11 10:54:19 +0800

    add vue dynamic router demo

--- vue-router/js/vue-router.demo.js
+++ vue-router/js/vue-router.demo.js
@@ -1,9 +1,17 @@
 const Foo = { template: '<div>foo</div>' }
 const Bar = { template: '<div>bar</div>' }
+const User = { template: '<div>User {{$route.params.id}}</div>',
+  watch: {
+    '$route'(to, from) {
+        console.log(from.path + "==>" + to.path);
+    }
+  }
+}
 
 const routes = [
   { path: '/foo', component: Foo },
-  { path: '/bar', component: Bar }
+  { path: '/bar', component: Bar },
+  { path: '/user/:id', component: User }
 ]
 
 const router = new VueRouter({
